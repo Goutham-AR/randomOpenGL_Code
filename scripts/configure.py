800f9f28b490ac39984671f6bc4c2523da045a1c
Goutham-AR <argouthu8400@gmail.com>
2021-06-24 18:04:15 +0530

    updated the configure script

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,12 +1,19 @@
 #! /usr/bin/env python
 
 import subprocess
+import argparse
 
+arg_parser = argparse.ArgumentParser(
+    description="preffered cmake generator")
+
+arg_parser.add_argument("-G", default="Ninja",
+                        nargs=1, metavar="Generator", dest="gen")
+
+args = arg_parser.parse_args()
 # cmake -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_INSTALL=OFF -S . -B build
-
 glfw_flags = ["-DGLFW_BUILD_DOCS=OFF", "-DGLFW_BUILD_TESTS=OFF",
               "-DGLFW_BUILD_EXAMPLES=OFF", "-DGLFW_INSTALL=OFF"]
 
-cmake_flags = ["-S.", "-Bbuild"]
+cmake_flags = ["-S.", "-Bbuild", "-G" + args.gen[0]]
 
 subprocess.run(["cmake"] + glfw_flags + cmake_flags)
