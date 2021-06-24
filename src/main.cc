bd8a1a5304de53e99a702d04e87848acfa236924
Goutham-AR <argouthu8400@gmail.com>
2021-06-24 22:49:14 +0530

    Refactoring

--- src/main.cc
+++ src/main.cc
@@ -6,194 +6,162 @@
 #include <GLFW/glfw3.h>
 #include <stb_image/stb_image.h>
 
-
+#include "window.hpp"
 #include "shader.hpp"
+#include "input_handler.hpp"
 
 
 
 
 void framebuffer_resize_callback(GLFWwindow* window, int new_width, int new_height);
-void process_input(GLFWwindow* window, Shader& program, float& current_i);
 
 int main()
 {
-    /* ------------------------------ */
-    /* GLFW and GLAD Init Part */
-    /* --------------------------- */
+	WindowProps window_properties{ "Opengl", 800, 600 };
 
-    /* initializing GLFW */
-    if (glfwInit() == GLFW_TRUE)
-    {
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#if defined(__APPLE__)
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    }
-
-    /* creating the window */
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
-    if (window == nullptr)
-    {
-        std::cerr << "Failed to create a window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    /* initializing GLAD */
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);     // redundant
-    /* Registering callbacks for window resizing */
-    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
-
-    /* ------------------------------ */
-    /* GLFW and GLAD Init Part */
-    /* --------------------------- */
-
-
-    Shader program{ "res/shaders/vertex.glsl", "res/shaders/fragment.glsl" };
-
-    program.use();
-    // program.set_float("h_offset", 0.3f);
+	Window window{ window_properties };
 
 
 
-    unsigned int texture[2];
-    glGenTextures(2, texture);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	/* initializing GLAD */
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-    int tex_width, tex_height, nr_channels;
-    unsigned char* data = stbi_load("res/textures/container.jpg", &tex_width, &tex_height, &nr_channels, 0);
-
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        stbi_image_free(data);
-    }
-    else
-    {
-        std::cerr << "Failed to load texture" << std::endl;
-    }
+	glViewport(0, 0, 800, 600);     // redundant
+	/* Registering callbacks for window resizing */
+	glfwSetFramebufferSizeCallback(window.get_window_ptr(), framebuffer_resize_callback);
 
 
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	Shader program{ "res/shaders/vertex.glsl", "res/shaders/fragment.glsl" };
 
-    stbi_set_flip_vertically_on_load(true);
-    data = stbi_load("res/textures/awesomeface.png", &tex_width, &tex_height, &nr_channels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        stbi_image_free(data);
-    }
-    else
-    {
-        std::cerr << "Failed to load texture" << std::endl;
-    }
+	program.use();
+	// program.set_float("h_offset", 0.3f);
+
+	InputHandler ih{ window.get_window_ptr() };
+
+	SmileyTexChangeDec a{ program };
+	SmileyTexChangeInc b{ program };
+	ExitCommand cmnd{ window.get_window_ptr() };
+
+	ih.bind_btndown(&b);
+	ih.bind_btnup(&a);
+	ih.bind_escape(&cmnd);
+
+	unsigned int texture[2];
+	glGenTextures(2, texture);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int tex_width, tex_height, nr_channels;
+	unsigned char* data = stbi_load("res/textures/container.jpg", &tex_width, &tex_height, &nr_channels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cerr << "Failed to load texture" << std::endl;
+	}
 
 
-    float vertices[] = {
-        // positions            // colors               // tex coords
-        0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,       1.0f, 1.0f,       // top right
-        0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,       1.0f, 0.0f,       // bottom right
-        -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,       0.0f, 0.0f,      // bottom left
-        -0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,       0.0f, 1.0f        //top left
-    };
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_set_flip_vertically_on_load(true);
+	data = stbi_load("res/textures/awesomeface.png", &tex_width, &tex_height, &nr_channels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cerr << "Failed to load texture" << std::endl;
+	}
 
 
-    unsigned int VAO, EBO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &EBO);
-    glGenBuffers(1, &VBO);
+	float vertices[] = {
+		// positions            // colors               // tex coords
+		0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,       1.0f, 1.0f,       // top right
+		0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,       1.0f, 0.0f,       // bottom right
+		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,       0.0f, 0.0f,      // bottom left
+		-0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 0.0f,       0.0f, 1.0f        //top left
+	};
 
-    glBindVertexArray(VAO);
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	unsigned int VAO, EBO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+	glGenBuffers(1, &VBO);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(0));
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+	glBindVertexArray(VAO);
 
-    program.use();
-    program.set_int("our_texture1", 0);
-    program.set_int("our_texture2", 1);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    auto current_i = 0.2f;
-    program.set_float("i", current_i);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    /* render loop */
-    while (!glfwWindowShouldClose(window))
-    {
-        process_input(window, program, current_i);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(0));
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-        /* rendering commands */
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture[0]);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture[1]);
-        program.use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	program.use();
+	program.set_int("our_texture1", 0);
+	program.set_int("our_texture2", 1);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	program.set_float("i", 0.2f);
 
-    glfwTerminate();
+	/* render loop */
+	while (!window.is_closed())
+	{
+		ih.handle_input();
+
+		/* rendering commands */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture[0]);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture[1]);
+		program.use();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		window.swap_buffer();
+		glfwPollEvents();
+	}
+
 }
 
 void framebuffer_resize_callback(GLFWwindow* window, int new_width, int new_height)
 {
-    glViewport(0, 0, new_width, new_height);
+	glViewport(0, 0, new_width, new_height);
 }
 
-void process_input(GLFWwindow* window, Shader& program, float& current_i)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
-    else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    {
-        if (current_i < 1.0f) current_i += 0.01f;
-        else current_i = 1.0f;
-        program.set_float("i", current_i);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    {
-        if (current_i > 0.0f) current_i -= 0.01f;
-        else current_i = 0.0f;
-        program.set_float("i", current_i);
-    }
-}
